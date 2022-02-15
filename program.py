72c4ce7be29f0fc63bc76ff4d21a287b4aa2f826
subhan <subhan97ahmed@gmail.com>
2022-02-16 01:08:50 +0500

    'Init'

---
+++ program.py
@@ -0,0 +1,122 @@
+# author Void(subhan ahmed)
+
+# file = open('input_data/a_an_example.in.txt', 'r')
+# file = open('input_data/b_basic.in.txt', 'r')
+# file = open('input_data/c_coarse.in.txt', 'r')
+file = open('input_data/d_difficult.in.txt', 'r')
+# file = open('input_data/e_elaborate.in.txt', 'r')
+lines = []
+# reading file
+for w in file.readlines():
+    lines.append(w.strip())
+
+customerNo = 1
+likeIngredients = {}
+dislikeIngredients = {}
+while customerNo <= len(lines) - 1:
+    # taking out likes
+    currentLikes = lines[customerNo].split(' ')
+    # scoring likes and storing them in dictionary
+    noOfLikes = int(currentLikes[0])
+    for i in range(1, noOfLikes + 1):
+        if currentLikes[i] in likeIngredients.keys():
+            likeIngredients[str(currentLikes[i])] = int(likeIngredients[str(currentLikes[i])]) + 1
+        else:
+            likeIngredients["" + currentLikes[i]] = 1
+
+    # dislikes
+    currentDislikes = lines[customerNo + 1].split(' ')
+
+    noOfDislikes = int(currentDislikes[0])
+    for i in range(1, noOfDislikes + 1):
+        if currentDislikes[i] in dislikeIngredients.keys():
+            dislikeIngredients[str(currentDislikes[i])] = int(dislikeIngredients[str(currentDislikes[i])]) + 1
+        else:
+            dislikeIngredients["" + currentDislikes[i]] = 1
+
+    customerNo = customerNo + 2
+
+print("like items with score")
+print(likeIngredients)
+print("dislike items with score")
+print(dislikeIngredients)
+# todo do add those items which is not disliked by anyone to increase score
+# popping out the dislike Ingredients if it is more disliked than liked
+resultIngredients = likeIngredients.copy()
+for key_of_dislikes in dislikeIngredients.keys():
+    for key_of_likes in likeIngredients.keys():
+        if key_of_likes == key_of_dislikes:
+            if likeIngredients[key_of_likes] <= dislikeIngredients[key_of_dislikes]:
+                resultIngredients.pop(key_of_likes)
+
+resultIngredients = list(resultIngredients.keys())
+resultIngredients.insert(0, len(resultIngredients))
+resultIngredientsString = ' '.join([str(elem) for elem in resultIngredients])
+# print(resultIngredientsString)
+
+file.close()
+# creating output file
+outputFile = open('output.txt', 'w')
+outputFile.write(resultIngredientsString)
+outputFile.close()
+
+
+#  for scoring output file locally
+# todo method is not accurate with a lot of customers
+def sim_result_score(inputData, resultItems):
+    noOfCustomers = inputData[0]
+    customers = []
+    itemNo = 1
+    while itemNo <= len(inputData) - 1:
+        # taking out likes
+        currentLike = inputData[itemNo].split(' ')
+        inputData[itemNo + 1].split(' ').pop(0)
+        currentDislike = inputData[itemNo + 1].split(' ')
+        currentLike.pop(0)
+        currentDislike.pop(0)
+        customers.append([currentLike, currentDislike])
+        itemNo = itemNo + 2
+
+    # separating each result item
+    resultItems = resultItems.split(" ")
+    resultItems.pop(0)
+    # print(resultItems)
+    score = 0
+    for customer in customers:
+        n = 0
+        while n < len(customer):
+            # for likes
+            likeFlag = False
+            dislikeFlag = False
+            counter = 0
+            for likeInfo in customer[n]:
+                for item in resultItems:
+                    if item == likeInfo:
+                        counter = counter + 1
+                if counter == len(customer[n]):
+                    likeFlag = True
+
+            # for dislikes
+            counter = 0
+            if len(customer[n + 1]) == 0:
+                dislikeFlag = True
+            else:
+                for dislikeInfo in customer[n + 1]:
+
+                    for item in resultItems:
+                        if item == dislikeInfo:
+                            counter = counter + 1
+                    if counter == len(customer[n + 1]):
+                        dislikeFlag = True
+            if likeFlag and dislikeFlag:
+                score = score + 1
+            n = n + 2
+
+    return int(score)
+
+
+# reading from output file for scoring
+file = open("output.txt", "r")
+line = file.readline()
+file.close()
+print("score " + str(sim_result_score(lines, str(line))))
